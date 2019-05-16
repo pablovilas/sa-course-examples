36a7312a8aa6e97a80a80f8a595fbb265b9d7e5e
Pablo Vilas <pablovilas89@gmail.com>
2019-05-15 21:04:07 -0300

    C7: Adds response time to ping/echo example

--- c7/ping-echo-module/lib/ping.js
+++ c7/ping-echo-module/lib/ping.js
@@ -19,25 +19,29 @@ module.exports = class Ping extends EventEmitter {
     ping() {
        this.createInterval();
     }
+    delay(init) {
+        return new Date().getTime() - init.getTime();
+    }
     doPing() {
+        let init = new Date();
         const self = this;
         const req = Http.request(this.options);
         req.on('response', (res) => {
             res.resume();
             res.on('end', function() {
                 if (res.statusCode >= 200 && res.statusCode < 300) {
-                    self.emit('success', `Successful ping to ${self.url()}: Status code is ${res.statusCode}`);
+                    self.emit('success', `[${self.delay(init)}ms] Successful ping to ${self.url()}: Status code is ${res.statusCode}`);
                 } else {
-                    self.emit('failure', `Error pinging ${self.url()}: Status code is ${res.statusCode}`);   
+                    self.emit('failure', `[${self.delay(init)}ms] Error pinging ${self.url()}: Status code is ${res.statusCode}`);   
                 }
             });
         });
         req.on('timeout', () => {
             req.destroy();
-            self.emit('failure', `Timeout pinging ${self.url()}`);
+            self.emit('failure', `[${self.delay(init)}ms] Timeout pinging ${self.url()}`);
         });
         req.on('error', (e) => {
-            self.emit('failure', `Error pinging ${self.url()}: ${e.message}`);
+            self.emit('failure', `[${self.delay(init)}ms] Error pinging ${self.url()}: ${e.message}`);
         });
         req.end();
     }
